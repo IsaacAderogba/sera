8ee61e77b21dbd1a291828d00b9249d2c31bdf48
Isaac Aderogba <isaacaderogba1@gmail.com>
2024-12-22 16:23:15 +0000

    feat(editor): progress on play button

---
+++ packages/editor/src/patterns/Editor/EditorPlayPauseButton.tsx
@@ -0,0 +1,51 @@
+import { IconButton } from "@radix-ui/themes";
+import { PlayerRef } from "@remotion/player";
+import { useCallback, useEffect, useState } from "react";
+import {
+  BackwardIcon,
+  ForwardIcon,
+  PauseIcon,
+  PlayIcon
+} from "@heroicons/react/24/outline";
+
+export interface EditorPlayPauseButtonProps {
+  playerRef: React.RefObject<PlayerRef | null>;
+}
+
+export const EditorPlayPauseButton: React.FC<EditorPlayPauseButtonProps> = ({
+  playerRef
+}) => {
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    const { current } = playerRef;
+    setPlaying(current?.isPlaying() ?? false);
+    if (!current) return;
+
+    const onPlay = () => {
+      setPlaying(true);
+    };
+
+    const onPause = () => {
+      setPlaying(false);
+    };
+
+    current.addEventListener("play", onPlay);
+    current.addEventListener("pause", onPause);
+
+    return () => {
+      current.removeEventListener("play", onPlay);
+      current.removeEventListener("pause", onPause);
+    };
+  }, [playerRef]);
+
+  const onToggle = useCallback(() => {
+    playerRef.current?.toggle();
+  }, [playerRef]);
+
+  return (
+    <IconButton onClick={onToggle} variant="ghost" style={{ margin: 0 }}>
+      {playing ? <PauseIcon width={16} /> : <PlayIcon width={16} />}
+    </IconButton>
+  );
+};
--- packages/editor/src/views/EditorView.tsx
+++ packages/editor/src/views/EditorView.tsx
@@ -1,12 +1,14 @@
-import { Player } from "@remotion/player";
-import { useMemo } from "react";
+import { Player, PlayerRef } from "@remotion/player";
+import { useMemo, useRef } from "react";
 import { Box } from "../components/Box";
 import { Flex } from "../components/Flex";
 import { useSelector } from "../providers/StoreContext";
 import { DefaultComposition } from "../remotion/DefaultComposition";
 import { calculateMetadata } from "../remotion/utilities";
+import { EditorPlayPauseButton } from "../patterns/Editor/EditorPlayPauseButton";
 
 export const EditorView: React.FC = () => {
+  const playerRef = useRef<PlayerRef>(null);
   const composition = useSelector(state => state.editor.composition);
   const metadata = useMemo(() => calculateMetadata(composition), [composition]);
 
@@ -22,6 +24,7 @@ export const EditorView: React.FC = () => {
       <Flex css={{ background: "$background", justifyContent: "center" }}>
         <Box className="no-drag" css={{ width: "400px", height: "300px" }}>
           <Player
+            ref={playerRef}
             controls
             component={DefaultComposition}
             inputProps={composition}
@@ -33,15 +36,23 @@ export const EditorView: React.FC = () => {
           />
         </Box>
       </Flex>
-      <Box
+      <Flex
+        className="no-drag"
         css={{
-          background: "$surface",
+          flexDirection: "column",
+          // background: "$translucent",
           height: "100%",
+          gap: "$sm",
           borderTop: "1px solid $border"
         }}
       >
-        Timeline
-      </Box>
+        <Flex css={{ padding: "$base", justifyContent: "space-between" }}>
+          <Box>Left</Box>
+          <EditorPlayPauseButton playerRef={playerRef} />
+          <Box>Right</Box>
+        </Flex>
+        <Box>Timeline</Box>
+      </Flex>
     </Flex>
   );
 };
