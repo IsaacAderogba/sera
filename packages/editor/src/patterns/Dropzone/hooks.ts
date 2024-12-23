0f35a6774f004a284f999218e284805a9f271b4d
Isaac Aderogba <isaacaderogba1@gmail.com>
2024-12-23 16:48:12 +0000

    feat(editor): add necessary dropzone hooks

--- packages/editor/src/patterns/Dropzone/hooks.ts
+++ packages/editor/src/patterns/Dropzone/hooks.ts
@@ -1,4 +1,11 @@
+import {
+  UseDraggableArguments,
+  useDraggable,
+  useDroppable
+} from "@dnd-kit/core";
+import { useSortable } from "@dnd-kit/sortable";
 import { useSelector } from "../../providers/StoreContext";
+import { useCombinedRefs } from "../../hooks/useManagedRefs";
 
 export const useDropzoneDragData = () => {
   return useSelector(state => {
@@ -6,3 +13,25 @@ export const useDropzoneDragData = () => {
     return Array.isArray(props) ? props : props ? [props] : [];
   });
 };
+
+export type DropzoneFlexibleProps = Pick<
+  UseDraggableArguments,
+  "id" | "data" | "disabled"
+>;
+
+export function useDropzoneFlexible(props: DropzoneFlexibleProps) {
+  const { isOver, setNodeRef: setDroppableNodeRef } = useDroppable(props);
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef: setDraggableNodeRef
+  } = useDraggable(props);
+
+  const setNodeRef = useCombinedRefs(setDraggableNodeRef, setDroppableNodeRef);
+  return { isOver, attributes, listeners, setNodeRef };
+}
+
+export const useDropzoneDraggable = useDraggable;
+export const useDropzoneDroppable = useDroppable;
+export const useDropzoneSortable = useSortable;
